33d3f37f5166c572ff8818f5d5ba83e82a6de181
MattWilAnti <144062315+MattWilAnti@users.noreply.github.com>
2024-01-23 10:01:46 -0600

    Add project files.

---
+++ Lab1aMWilson/Lab1aMWilson.cpp
@@ -0,0 +1,17 @@
+//THROW THE CHEESE!
+//ASDF
+//Weeee
+//Solve the World's problems!
+
+#include<iostream> //this "header file" allows the use of cout 
+					//(put Characters OUT - to the terminal) 
+
+using namespace std; //Standard
+
+int main()
+{
+	//Automatically indents.
+	cout << "Hello, World?" << endl; /*"endl" and "\n" creates
+	a new line within a function in code.*/
+	return 0;
+}
